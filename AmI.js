498476c610d4c2900b0e2ecb4195bd216390b3eb
Gabriel Lundgren <glundgren93@gmail.com>
2017-11-13 23:13:54 -0200

    adding more predicates

--- AmI.js
+++ AmI.js
@@ -2,9 +2,18 @@
 
 let amI = { not: {} };
 
-function string(val) {
+const string = val => {
   return typeof val === "string";
-}
+};
+const number = val => {
+  return typeof val === "number";
+};
+const boolean = val => {
+  return typeof val === "boolean";
+};
+const symbol = val => {
+  return typeof val === "symbol";
+};
 
 let addPredicate = (name, predicate) => {
   amI[name] = predicate;
@@ -14,6 +23,30 @@ let addPredicate = (name, predicate) => {
   };
 };
 
-addPredicate("string", val => typeof val === "string");
+/**
+ * Primitives
+ */
+addPredicate("string", val => string(val));
+addPredicate("number", val => number(val));
+addPredicate("boolean", val => boolean(val));
+addPredicate("symbol", val => symbol(val));
+addPredicate("undefined", val => val === undefined);
+addPredicate("null", val => val === null);
+addPredicate("primitive", val => {
+  return (
+    string(val) ||
+    number(val) ||
+    boolean(val) ||
+    symbol(val) ||
+    val === undefined ||
+    val === null
+  );
+});
+
+addPredicate("NaN", val => isNaN(val));
+addPredicate("function", val => typeof val === "function");
+addPredicate("function", val => typeof val === "function");
+addPredicate("function", val => typeof val === "function");
+addPredicate("function", val => typeof val === "function");
 
 module.exports = amI;
--- test/am-i.test.js
+++ test/am-i.test.js
@@ -4,11 +4,75 @@ const amI = require("../AmI");
 const expect = chai.expect;
 
 describe("Primitives", () => {
-  it("am i string", () => {
+  it("am i a string", () => {
     expect(amI.string("gjepaiga")).to.equal(true);
   });
 
-  it("am i not string", () => {
+  it("am i not a string", () => {
     expect(amI.not.string("faefae")).to.equal(false);
   });
+
+  it("am i a number", () => {
+    expect(amI.number(9)).to.equal(true);
+  });
+  it("am i not a number", () => {
+    expect(amI.number("number")).to.equal(false);
+  });
+
+  it("am i a boolean", () => {
+    expect(amI.boolean(false)).to.equal(true);
+  });
+  it("am i not a boolean", () => {
+    expect(amI.boolean("false")).to.equal(false);
+  });
+
+  it("am i a symbol", () => {
+    expect(amI.symbol(Symbol("symbol"))).to.equal(true);
+  });
+
+  it("am i not a symbol", () => {
+    expect(amI.symbol("Symbol")).to.equal(false);
+  });
+
+  it("am i undefined", () => {
+    let x;
+    expect(amI.undefined(x)).to.equal(true);
+  });
+
+  it("am i not undefined", () => {
+    let x = 1;
+    expect(amI.undefined(x)).to.equal(false);
+  });
+
+  it("am i null", () => {
+    let x = null;
+    expect(amI.null(x)).to.equal(true);
+  });
+
+  it("am i not null", () => {
+    let x;
+    expect(amI.null(x)).to.equal(false);
+  });
+
+  it("am i a primitive", () => {
+    expect(amI.primitive("primitive")).to.equal(true);
+  });
+
+  it("am i not a primitive", () => {
+    expect(amI.primitive({})).to.equal(false);
+  });
+});
+
+describe("Objects", () => {
+  it("am i a function", () => {
+    let fn = () => {};
+
+    expect(amI.function(fn)).to.equal(true);
+  });
+
+  it("am i not a function", () => {
+    expect(amI.function("fn")).to.equal(false);
+  });
+
+  
 });
